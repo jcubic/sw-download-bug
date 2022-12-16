2e5e549af41816004f581e6d64a84e364d8bc53e
Jakub Jankiewicz <jcubic@onet.pl>
2022-12-16 20:29:52 +0100

    init commit

---
+++ sw.js
@@ -0,0 +1,42 @@
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(new Promise((resolve, reject) => {
+    try {
+      const req = event.request;
+      const url = new URL(req.url);
+      const path = url.pathname;
+      if (path.match(/__download__/)) {
+        download().then(resolve).catch(error => {
+          resolve(make_error(error));
+        });
+      }
+      if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+        return;
+      }
+      //request = credentials: 'include'
+      fetch(event.request).then(resolve).catch(reject);
+    } catch(error) {
+      resolve(make_error(error));
+    }
+  }));
+});
+
+function response(data, options) {
+  const blob = new Blob([data], {
+    type: 'text/plain'
+  });
+  return new Response(blob, options);
+}
+
+async function download() {
+  const text = await fetch('hacker.txt').then(res => res.text());
+  const headers = {
+    'Content-Disposition': 'attachment; filename="hacker.txt"'
+  };
+  return response(text, { headers });
+}
+
+function make_error(err) {
+  return response(error.message + '\n' + error.stack);
+}
